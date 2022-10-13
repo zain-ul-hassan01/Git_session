7f4d232e46374c872a274f4e57c9285b8df3e8b1
zain hassan <zain.hassan@devsinc.com>
2022-10-13 16:25:42 -0700

    Zain/02: Added sample ruby files

---
+++ sample.rb
@@ -0,0 +1,5 @@
+puts "Hello World"
+arr = [1,2,3]
+arr.each do |ar|
+    puts "array: #{ar}"
+end
---
+++ sample1.rb
@@ -0,0 +1,6 @@
+sample = "sample"
+sample1 = "sample1"
+
+if sample.count(sample1) > 0
+    puts "Sample1 is a substring."
+end
